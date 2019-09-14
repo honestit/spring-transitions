438f78267a4fefe0e1ea1898cecc1f92f434c1ef
honestit <michal.kupisinski@gmail.com>
2019-09-14 06:58:33 +0200

    Podstawowa konfiguracja autoryzacji

--- spring-with-security-and-test-user/src/main/java/pl/honestit/demos/spring/config/SecurityLayerConfiguration.java
+++ spring-with-security-and-test-user/src/main/java/pl/honestit/demos/spring/config/SecurityLayerConfiguration.java
@@ -3,6 +3,7 @@ package pl.honestit.demos.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -34,5 +35,15 @@ public class SecurityLayerConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .antMatchers("/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .and()
+            .httpBasic();
     }
 }
