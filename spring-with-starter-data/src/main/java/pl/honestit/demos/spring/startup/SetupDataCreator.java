24dd23313072648667883b007b5f13f348377a3c
honestit <michal.kupisinski@gmail.com>
2019-09-14 13:11:19 +0200

    Spring-with-starter-data: Tworzenie użytkownikó w różnych rolach jeżeli nie istnieją

--- spring-with-starter-data/src/main/java/pl/honestit/demos/spring/startup/SetupDataCreator.java
+++ spring-with-starter-data/src/main/java/pl/honestit/demos/spring/startup/SetupDataCreator.java
@@ -5,6 +5,11 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.honestit.demos.spring.model.dal.repositories.UserRepository;
+import pl.honestit.demos.spring.model.entities.user.UserEntity;
+import pl.honestit.demos.spring.model.entities.user.UserRole;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class SetupDataCreator implements ApplicationRunner {
@@ -19,6 +24,22 @@ public class SetupDataCreator implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        createUserIfNotExists("user", "pass", "user@honestit.pl", "USER");
+        createUserIfNotExists("manager", "pass", "manager@honestit.pl", "MANAGER", "MANAGER");
+        createUserIfNotExists("admin", "pass", "admin@honestit.pl", "ADMIN");
+    }
 
+    private void createUserIfNotExists(String username, String password, String email, String... roles) {
+        userRepository.findByUsername(username)
+                .ifPresentOrElse(System.out::println,
+                        () -> {
+                            UserEntity user = new UserEntity();
+                            user.setUsername(username);
+                            user.setPassword(passwordEncoder.encode(password));
+                            user.setEmail(email);
+                            user.setEnabled(true);
+                            user.getRoles().addAll(Stream.of(roles).map(UserRole::new).collect(Collectors.toSet()));
+                            userRepository.save(user);
+                        });
     }
 }
