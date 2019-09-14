d5ff0515d0ae1a40cab345a2a3e550f62d602566
honestit <michal.kupisinski@gmail.com>
2019-09-14 13:03:45 +0200

    Spring-with-starter-data: Podstawowe zależności do innych komponentów

--- spring-with-starter-data/src/main/java/pl/honestit/demos/spring/startup/SetupDataCreator.java
+++ spring-with-starter-data/src/main/java/pl/honestit/demos/spring/startup/SetupDataCreator.java
@@ -2,10 +2,20 @@ package pl.honestit.demos.spring.startup;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import pl.honestit.demos.spring.model.dal.repositories.UserRepository;
 
 @Component
 public class SetupDataCreator implements ApplicationRunner {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public SetupDataCreator(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
