c73fe68f0d397e82fd852f1492370489eeaf0095
honestit <michal.kupisinski@gmail.com>
2019-09-14 06:54:56 +0200

    Konfiguracja użytkowników testowych przechowywanych w pamięci

--- spring-with-security-and-test-user/src/main/java/pl/honestit/demos/spring/config/SecurityLayerConfiguration.java
+++ spring-with-security-and-test-user/src/main/java/pl/honestit/demos/spring/config/SecurityLayerConfiguration.java
@@ -2,6 +2,7 @@ package pl.honestit.demos.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,4 +19,13 @@ public class SecurityLayerConfiguration extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}pass").roles("USER")
+                .and()
+                .withUser("manager").password("{noop}pass").roles("USER", "MANAGER")
+                .and()
+                .withUser("admin").password("{noop}s3cr3t").roles("ADMIN");
+    }
 }
