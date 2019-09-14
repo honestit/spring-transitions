bc0c551c425ebf57e7e23a1dbc61a4a6b3bb8838
honestit <michal.kupisinski@gmail.com>
2019-09-14 13:03:06 +0200

    Spring-with-starter-data: SetupDataCreator uruchamiany podczas startu aplikacji

---
+++ spring-with-starter-data/src/main/java/pl/honestit/demos/spring/startup/SetupDataCreator.java
@@ -0,0 +1,14 @@
+package pl.honestit.demos.spring.startup;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetupDataCreator implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
