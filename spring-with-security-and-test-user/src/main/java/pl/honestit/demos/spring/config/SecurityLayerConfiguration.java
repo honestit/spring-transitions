5996cd167a788958833132c670437b8286a65a73
honestit <michal.kupisinski@gmail.com>
2019-09-14 06:53:17 +0200

    Wystawienie uniwersalnego bean'a do szyfrowania haseł

--- spring-with-security-and-test-user/src/main/java/pl/honestit/demos/spring/config/SecurityLayerConfiguration.java
+++ spring-with-security-and-test-user/src/main/java/pl/honestit/demos/spring/config/SecurityLayerConfiguration.java
@@ -1,7 +1,10 @@
 package pl.honestit.demos.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 /*
@@ -9,5 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
     przez Spring Boot
  */
 public class SecurityLayerConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
