79e20c2cddc083dd32cac546f062e87202228dfb
honestit <michal.kupisinski@gmail.com>
2019-09-15 14:57:31 +0200

    Spring-with-jsp-views: Podstawowy kontroler do obsługi rejestracji użytkownika

---
+++ spring-with-jsp-views/src/main/java/pl/honestit/demos/spring/web/controllers/jsp/RegistrationController.java
@@ -0,0 +1,9 @@
+package pl.honestit.demos.spring.web.controllers.jsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jsp/register")
+public class RegistrationController {
+}
