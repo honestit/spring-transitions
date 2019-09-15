f3806323fd8072b50cef64d1d863d7f34be09500
honestit <michal.kupisinski@gmail.com>
2019-09-15 15:26:09 +0200

    Spring-with-jsp-views: Struktura stron wspierana na poziomie poprawności typów

---
+++ spring-with-jsp-views/src/main/java/pl/honestit/demos/spring/web/utils/Pages.java
@@ -0,0 +1,10 @@
+package pl.honestit.demos.spring.web.utils;
+
+public class Pages {
+
+    public static class Registration {
+
+        public static final String FORM = "/registration/form";
+        public static final String SUCCESS = "/registration/success";
+    }
+}
