40b0f79f88aa0a42b099b35a7e37db36f44f11bb
honestit <michal.kupisinski@gmail.com>
2019-09-15 13:20:14 +0200

    Sping-with-repositories-and-advanced-usage: Wdrożenie do istniejącego repozytorium obsługi projekcji

--- spring-with-repositories-and-advanced-usage/src/main/java/pl/honestit/demos/spring/model/dal/repositories/UserRepository.java
+++ spring-with-repositories-and-advanced-usage/src/main/java/pl/honestit/demos/spring/model/dal/repositories/UserRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long>, ProjectionsRepository<Long>
+{
 
     /**
      * Pobieranie użytkownika po nazwie.
