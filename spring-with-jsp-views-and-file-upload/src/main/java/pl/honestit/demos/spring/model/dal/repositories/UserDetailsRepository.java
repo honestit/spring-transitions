6839e6a3d3fe4ff137ac8cfcf9e226e0ea37ae2e
honestit <michal.kupisinski@gmail.com>
2019-09-20 09:41:47 +0200

    Spring-with-jsp-views-and-file-upload: Dodanie meotyd do repozytorium dociągającej dane pliku

--- spring-with-jsp-views-and-file-upload/src/main/java/pl/honestit/demos/spring/model/dal/repositories/UserDetailsRepository.java
+++ spring-with-jsp-views-and-file-upload/src/main/java/pl/honestit/demos/spring/model/dal/repositories/UserDetailsRepository.java
@@ -1,7 +1,11 @@
 package pl.honestit.demos.spring.model.dal.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.honestit.demos.spring.model.entities.user.UserDetailsEntity;
 
 public interface UserDetailsRepository extends JpaRepository<UserDetailsEntity, Long> {
+
+    @EntityGraph(attributePaths = {"profileFile.data"})
+    UserDetailsEntity getWithProfileFileByOwnerUsername(String username);
 }
