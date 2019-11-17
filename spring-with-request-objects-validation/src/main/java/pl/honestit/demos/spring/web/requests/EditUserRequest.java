4b868e796a3db0235bb6f42b48738dac33fc4e00
honestit <michal.kupisinski@gmail.com>
2019-11-17 13:31:14 +0100

    Spring-with-request-objects-validation: Wdrożenie adnotacji walidacyjnych

--- spring-with-request-objects-validation/src/main/java/pl/honestit/demos/spring/web/requests/EditUserRequest.java
+++ spring-with-request-objects-validation/src/main/java/pl/honestit/demos/spring/web/requests/EditUserRequest.java
@@ -3,15 +3,17 @@ package pl.honestit.demos.spring.web.requests;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.pl.PESEL;
 
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor @AllArgsConstructor
 public class EditUserRequest {
-
+    
     private String firstName;
     private String lastName;
+    @PESEL
     private String pesel;
     private String dateOfBirth;
 
--- spring-with-request-objects-validation/src/main/java/pl/honestit/demos/spring/web/requests/RegistrationRequest.java
+++ spring-with-request-objects-validation/src/main/java/pl/honestit/demos/spring/web/requests/RegistrationRequest.java
@@ -5,13 +5,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 @ToString(exclude = {"password", "rePassword"})
 @NoArgsConstructor @AllArgsConstructor
 public class RegistrationRequest {
 
+    @NotBlank
     private String username;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 8)
     private String password;
+    @NotBlank
+    @Size(min = 8)
     private String rePassword;
 }
