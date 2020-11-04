5eea123b999da49d1dcbf4a74599587b1ca5ea5a
honestit <michal.kupisinski@gmail.com>
2020-11-04 06:39:12 +0100

    Own class representing logged user (for MVC and oauth2)

---
+++ spring-with-jsp-views-and-oauth2-facebook/src/main/java/pl/honestit/demos/spring/security/LoggedUser.java
@@ -0,0 +1,72 @@
+package pl.honestit.demos.spring.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Własna implementacja klasy reprezentującej zalogowanego użytkownika łącząca użytkownika
+ * logowanego przy użyciu podstawowego Security (MVC) i oauth2
+ */
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class LoggedUser implements UserDetails, OAuth2User {
+
+    private String username;
+    private String password;
+    private Boolean active;
+    private Set<? extends GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+
+    @Override
+    public String getName() {
+        return username;
+    }
+}
